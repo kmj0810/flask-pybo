99f4f830762f9eae3dfe1ba1e4b4246a35a4b74c
kmj0810 <munright1229@gmail.com>
2026-09-18 17:57:22 +0900

    2026.09.18

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-from flask import Blueprint, redirect, url_for, request, render_template, g
+from flask import Blueprint, redirect, url_for, request, render_template, g, flash
 
+from pybo.models import Answer
 from pybo import db
 from pybo.forms import AnswerForm
 from pybo.models import Question, Answer
@@ -20,10 +21,41 @@ def create(question_id):
         content = form.content.data     # 폼모듈로 입력된 값 받을 때
         print(content)
 
-        answer = Answer(question= question, content=content, create_date=datetime.now())
+        answer = Answer(question= question, content=content, create_date=datetime.now(), user=g.user)
         db.session.add(answer)
         db.session.commit()
 
         return redirect(url_for('question.detail', question_id=question_id))
 
     return render_template('question/question_detail.html', question=question, form=form)
+
+@bp.route('/modify/<int:answer_id>', methods=['GET','post'])
+@login_required
+def modify(answer_id):
+    answer=Answer.query.get_or_404(answer_id)
+    if g.user != answer.user:
+        flash('수정권한이 없습니다.')
+        return redirect(url_for('question.detail', question_id=answer.question_id))
+    if request.method == 'POST':
+        form = AnswerForm()
+        if form.validate_on_submit():
+            form.populate_obj(answer)
+            answer.modify_date = datetime.now()
+            db.session.commit()
+            return redirect(url_for('question.detail', question_id=answer.question_id))
+    else:
+        form = AnswerForm(obj=answer)
+    return render_template('answer/answer_form.html', form=form)
+
+
+@bp.route('/delete/<int:answer_id>')
+@login_required
+def delete(answer_id):
+    answer=Answer.query.get_or_404(answer_id)
+    question_id=answer.question_id
+    if g.user != answer.user:
+        flash('삭제권환이 없습니다.')
+    else:
+        db.session.delete(answer)
+        db.session.commit()
+    return redirect(url_for('question.detail', question_id=question_id))
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,22 +1,35 @@
 from datetime import datetime
 
-from flask import Blueprint, render_template, request, redirect, url_for, g
+from flask import Blueprint, render_template, request, redirect, url_for, g, flash
 
 from pybo import db
 from pybo.forms import QuestionForm, AnswerForm
-from pybo.models import Question
+from pybo.models import Question, Answer, User
 from pybo.views.auth_viewe import login_required
 
 # Blueprint: 라우팅 함수를 체계적으로 관리
 bp = Blueprint('question', __name__, url_prefix='/question')
 
-@bp.route('/list')
+@bp.route('/list/')
 def _list():
     page = request.args.get('page', default=1, type=int)  # url parameter ?page=1
+    kw = request.args.get('kw', default='', type=str) # 검색 키쿼드 추가
     question_list = Question.query.order_by(Question.create_date.desc())
-    question_list = question_list.paginate(page=page, per_page=10) # 페이지 구현
-
-    return render_template('question/question_list.html', question_list=question_list)
+    if kw:
+        search = f'%{kw}%'
+        sub_query = db.session.query(Answer.question_id, Answer.content, User.username) \
+            .join(User, Answer.user_id == User.id).subquery()
+        question_list = question_list.join(User) \
+            .outerjoin(sub_query, sub_query.c.question_id == Question.id) \
+            .filter(Question.subject.ilike(search) |  # 질문제목
+                    Question.content.ilike(search) |  # 질문내용
+                    User.username.ilike(search) |  # 질문작성자
+                    sub_query.c.content.ilike(search) |  # 답변내용
+                    sub_query.c.username.ilike(search)  # 답변작성자
+                    ) \
+            .distinct()
+    question_list = question_list.paginate(page=page, per_page=10)    # 페이지 구현
+    return render_template('question/question_list.html', question_list=question_list, page=page, kw=kw)
 
 @bp.route('/detail/<int:question_id>')
 def detail(question_id):
@@ -34,3 +47,35 @@ def create():
         db.session.commit()
         return redirect(url_for('main.index'))
     return render_template('question/question_form.html', form=form)
+
+@bp.route('/modify/<int:question_id>', methods=['GET', 'POST'])
+@login_required
+def modify(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        flash('수정권한이 없습니다.')
+        return redirect(url_for('question.detail', question_id=question_id))
+    if request.method == 'POST':  # post 요청
+        form = QuestionForm()
+        if form.validate_on_submit():
+            form.populate_obj(question)
+            question.modify_date = datetime.now()
+            db.session.commit()
+            return redirect(url_for('question.detail', question_id=question_id))
+    else: # get요청
+        form = QuestionForm(obj=question)
+
+
+    return render_template('question/question_form.html', form=form)
+
+@bp.route('/delete/<int:question_id>')
+@login_required
+def delete(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        flash('삭제권한이 없습니다.')
+        return redirect(url_for('question.detail', question_id=question_id))
+    db.session.delete(question)
+    db.session.commit()
+    return redirect(url_for('question._list'))
+
